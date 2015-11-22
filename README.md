23c0fde1df6104b23599d986bfe5a0b907166412
publicjoe62 <publicjoe62@gmail.com>
2015-11-22 20:10:28 +0100

    add some content to LogHelper class

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # LogHelper
 
-enter my description here
+The class LogHelper is used to output logging information.
+
+## Methods
+Nachfolgend die Liste der öffentlichen Methoden von LogHelper().
+
+### Constructor
+* __LogHelper()__ 
+    
+    Ein Konstruktoraufruf ohne Parameter lenkt die Ausgaben auf die Konsole um und als Log-Level wird "ALL" 
+    angewendet. Als Zeitstempel wird das Format "dd.mm.yy hh:mm:ss.msec" verwendet.
+
+
+* LogHelper(outputfilename)
+
+
+### log(message)
+Ausgabe einer Log Meldung
+
+### close()
+Falls die Ausgabe in eine Datei umgelenkt wurde, wird die Datei geschlossen.
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 __author__ = "Public Joe62"
 
+import sys
+from datetime import datetime
+
 def singleton(class_):
   instances = {}
   def getinstance(*args, **kwargs):
@@ -11,31 +14,74 @@ def singleton(class_):
   return getinstance
 
 @singleton
-class MyClass():
+class LogHelper():
     value = 1
 
-    def getValue(self):
-        return self.value
+    _isconsole = False
+    _isfile = False
+    _isfileopened = False
+    _filename = ""
+    _file = None
+    _timestampformat = "%d.%m.%y %H:%M:%S.%f"
 
-    def setValue(self, xvalue):
-        self.value = xvalue
+    def __init__(self, paramfilename="CONSOLE"):
+        print "LogHelper::constructor"
+
+        if paramfilename == "CONSOLE":
+            self._isconsole = True
+        else:
+            # output redirected to file
+            #
+            self._isfile = True
+            self._filename = paramfilename
+
+        return
+
+    def _logopenfile(self):
+        if self._isfile:
+            self._file = open(self._filename, 'w')
+            self._isfileopened = True
+
+        else:
+            print "LogHelper::_logopenfile: _isfile=True expected!"
+            raise Exception
+        return
+
+    def log(self, strMsg):
+
+        # add timestamp to strMsg
+        #
+        strMsg = datetime.now().strftime(self._timestampformat) + " " + strMsg
+        if self._isconsole:
+            print strMsg
+        elif self._isfile:
+            # open file if necessary
+            #
+            if not self._isfileopened:
+                self._logopenfile()
+            # write string to file
+            #
+            self._file.write("%s\r\n" % (strMsg))
+        else:
+            print "LogHelper::log: self._isconsole or self._isfile expected!"
+            raise Exception
+        return
+
+    def close(self):
+        if self._isfileopened:
+            self._file.close()
+            self._isfileopened = False
+
         return
 
     pass
 
-class LogHelper:
-
-    def __init__(self):
-        return
-
 
 if __name__ == "__main__":
     print "loghelper.py::main::start"
-    m = MyClass()
-    print "wert 1 = %d" % m.getValue()
-    m.setValue(3)
-    n = MyClass()
-    print "wert 4 = %d" % n.getValue()
-
+    m = LogHelper("out2.txt")
+    m.log("msg1")
+    m.log("msg2")
+    m.close()
     print "loghelper.py::main::end"
 
